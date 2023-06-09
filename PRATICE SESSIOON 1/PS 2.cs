7965c77e3475c1c8d7f2630209e39fb19fc4645a
Pratik <Pratik@DESKTOP-9189TTI>
2023-06-09 12:14:26 +0530

    Add project files.

---
+++ PRATICE SESSIOON 1/PS 2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRATICE_SESSIOON_1
+{
+    internal class PS_2
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 5, num2 = 5;
+            Console.WriteLine(num1  < num2);
+            Console.WriteLine(num1 <= num2);
+
+            Console.WriteLine(num1 > num2);
+            Console.WriteLine(num2 >= num1);
+
+            Console.WriteLine(num1 == num2);
+            Console.WriteLine(num1 != num2);
+        }
+    }
+}
---
+++ PRATICE SESSIOON 1/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+int num1 = 5, num2 = 2, result1, result2;
+
+result1 = num1/num2;
+result2 = num1 % num2;
+Console.WriteLine($"division is {result1}");
+Console.WriteLine($"mod is {result2}");
